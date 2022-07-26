d07085c61af8ba08ae928322d13ce30fabcda01d
Nam Tran Huy <namtran@phs.vn>
2022-07-26 11:50:02 +0700

    2022-07-26

--- automation/risk_management/__init__.py
+++ automation/risk_management/__init__.py
@@ -1,6 +1,6 @@
 from automation import *
 
-dept_folder = r'C:\Users\hiepdang\Shared Folder\Risk Management\Report'
+dept_folder = r'C:\Users\namtran\Share Folder\Risk Management\Report'
 
 def get_info(
     periodicity:str,
